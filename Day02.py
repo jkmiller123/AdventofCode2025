12ace0347cffd1aa9da6eaa629585e95a274f165
Kevin Miller <jkmiller123@gmail.com>
2025-12-03 20:55:54 -0500

    Day 2 solved for both stars

--- Day02.py
+++ Day02.py
@@ -1,5 +1,5 @@
 
-testing = True   # used for debugging messages and to pick the file
+testing = False   # used for debugging messages and to pick the file
 stage_2 = True    # used for more complex coding changes
 aocDay = '02'
 file2read = 'Data/Day'+aocDay+'_Sample.txt' if testing else 'Data/Day'+aocDay+'_01.txt'
@@ -30,29 +30,45 @@ def part_01(data_list):
             lowrange += 1
     print(f'Total Invalids {sum_invalids}')
 
-
+def find_factors(number: object) -> object:
+    """
+    Finds all factors of a given positive integer.
+    Args:
+        number: A positive integer.
+    Returns:
+        A list containing all factors of the input number.
+    """
+    factors = []
+    for i in range(1, number + 1):
+        if number % i == 0:
+            factors.append(i)
+    return factors
 
 def part_02(data_list):
     sum_invalids = 0
     for items in data_list:
         lowrange, highrange = map(int, items.split('-'))
+        list_of_invalids = set()   # need to create a list to remove duplicates, in sample data 22222 would be added to list 3 times. for 222220-222224
         while (lowrange <= highrange):
             lowstring = str(lowrange)
-            sizeofLowrange = len(lowstring) // 2
-            for numdigits in range(sizeofLowrange):
-                searchstring = lowstring[:numdigits + 1]
-                if lowstring.count(searchstring) > 1 and len(lowstring) % lowstring.count(searchstring) == 0:
-                    print(f'search string = {searchstring} for {lowstring}')
-                numdigits += 1
-            # if sizeofLowrange % 2 == 0:
-            #     middle = sizeofLowrange // 2
-            #     leftside = lowstring[:middle]
-            #     rightside = lowstring[middle:]
-            #     if leftside == rightside:
-            #         sum_invalids += lowrange
-            #         print(f'Found invalid {lowrange}')
+            # split into factors to list the repeatable groups 
+            factorsList = find_factors(len(lowstring))
+            for factors in factorsList:
+                part = lowstring[0:factors]
+                num_parts = lowstring.count(part)
+                num_parts_total = int(len(lowstring) / factors)
+                if num_parts == num_parts_total and num_parts_total > 1:
+                    list_of_invalids.add(part*num_parts_total)
+                   # print(f'Found an invalid {part*num_parts_total} of {lowstring}')
             lowrange += 1
-    print(f'Total Invalids {sum_invalids}')
+        for invalids in list_of_invalids:
+            sum_invalids += int(invalids)
+
+        print(f'List of invalids {list_of_invalids} for {items}')
+    print(f'Total of invalids {sum_invalids}')
+
+    4174379265
+    4174823709
 
 
 data_list = read_parse_comma(a_file=file2read)
